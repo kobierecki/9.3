faa9abde13df229bca58909a6dbf757bc4a249bb
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2017-11-29 12:48:45 +0100

    9.3

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+var dinosaur = "triceraptos";
+var dinosaur = dinosaur.toUpperCase();
+var text = text.replace("Velociraptor", dinosaur);
+var text = text.slice(0, text.length / 2);
+
+console.log(text);
